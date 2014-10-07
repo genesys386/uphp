8129481357385531ebfaafd85662c68a127eb37f
Bernhard Bösl <bernhard.boesl@gmail.com>
2014-10-07 17:42:06 +0200

    Readjust structure to fit new ideas ;)

--- uphp/bootstrap.php
+++ uphp/bootstrap.php
@@ -4,10 +4,6 @@ namespace it\boesl\uphp;
 define('UPHP_DIR', __DIR__);
 
 require UPHP_DIR . '/src/it/boesl/uphp/AutoloaderFactory.php';
-
 spl_autoload_register(
     AutoloaderFactory::psr4('it\\boesl\\uphp\\', UPHP_DIR . '/src/it/boesl/uphp')
-);
-spl_autoload_register(
-    AutoloaderFactory::psr4('app\\', UPHP_DIR . '/../app/src/')
 );
